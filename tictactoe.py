23de2d5d792d906d676029af581825b6e99d247b
Russell Villanueva <villanuevarussell@yahoo.com>
2019-06-15 14:29:24 -0700

    First Commit

---
+++ tictactoe.py
@@ -0,0 +1,154 @@
+#Function to Show Board
+def display_board(board):
+    print(f"     |   |   ")
+    print(f"   {board[7]} | {board[8]} | {board[9]} ")
+    print(f"     |   |  ")
+    print('---------------')
+    print(f"     |   |   ")
+    print(f"   {board[4]} | {board[5]} | {board[6]} ")
+    print(f"     |   |  ")
+    print('---------------')
+    print(f"     |   |   ")
+    print(f"   {board[1]} | {board[2]} | {board[3]} ")
+    print(f"     |   |  ")
+    pass
+
+#function for user to select a marker
+def choose_marker():
+	#Condition for correct Selection
+	correctSelection = False
+
+	#uses global player 1 and 2 variables
+	global player1
+	global player2
+
+	#choose marker for first player, while loop forces user to choose a correct selection
+	player1 = input("Please pick a marker 'X' or 'O': ")
+	while correctSelection == False:
+		if player1 == 'X' or player1 == 'O':
+			correctSelection = True
+			break
+		else:
+			player1 = input("Please enter a valid selection. 'X' or 'O': ")
+
+	#Second player gets remaining option
+	if player1 == 'X':
+		player2 = 'O'
+	else:
+		player2 ='X'
+
+
+def player_input(board,player):
+    #selection flag to ensure user makes a correct selection and that it has not been selected yet 
+    correctSelection = False
+    display_board(board)
+    playerSelection = int (input(f"{player} Please select a number 1-9 that is not yet selected: "))
+
+    #runs through specific conditions to check if it is a valid input
+    while correctSelection == False:
+    	#ensure number is greater than 0 and less than 10
+        if playerSelection >= 10 or playerSelection == 0:
+            display_board(board)
+            print("That number is out of range")
+            playerSelection = int (input(f"{player} Please select a number 1-9 that is not yet selected: "))
+        #ensures that the selection has not been selected yet
+        elif board[playerSelection] == 'X' or board[playerSelection] == 'O':
+            display_board(board)
+            print("That number has been selected")
+            playerSelection = int (input(f"{player} Please select a number 1-9 that is not yet selected: ")) 
+        #allows changes to the board
+        else:
+            board[playerSelection] = player
+            correctSelection == True
+            break
+
+#function to check if there is a winner
+def win_check(board, player):
+	#count is number to check if all the spots have been used
+	count = 0
+	#loops through entire board to check if all the spots have been taken, count is added if spot is taken
+	for position in range(len(board)):
+		if board[position] == 'X' or board[position] == 'O':
+			count += 1
+	#if there are 9 spots taken then the game is a draw
+	if count == 9:
+		print("Game is a draw!")
+		return True
+
+		
+	#if any of these conditions are true, the player wins
+	if (board[1] == player and board[2] == player and board[3] == player) or (board[4] == player and board[5] == player and board[6] == player) or (board[7] == player and board[8] == player and board[9] == player) or (board[1] == player and board[4] == player and board[7] == player)  or (board[2] == player and board[5] == player and board[8] == player)  or (board[3] == player and board[6] == player and board[9] == player)  or (board[3] == player and board[5] == player and board[7] == player) or (board[1] == player and board[5] == player and board[9] == player) :
+		print(f"Player {player} wins!")
+		return True
+	else:
+		return False
+        
+
+#asks user if they want to play again
+def replay():
+	correctReplaySlection = False
+	while correctReplaySlection == False:
+		if replayGame == 'Y':
+			correctReplaySlection = True
+			return True          
+		elif replayGame == 'N':
+			return False
+		else:
+			print("Do you want to play again?")
+			player1 = input("Please enter a valid selection. 'Y' or 'N': ")
+
+
+
+
+
+replayGame = True
+while replayGame == True:
+
+	print("Welcome to Tic Tac Toe!")
+
+	#Condition for game loop
+	gameOver = False
+
+
+	#creates game board
+	gameBoard = [0,1,2,3,4,5,6,7,8,9]
+	player1 = None
+	player2 = None
+
+	choose_marker()
+
+
+	#takes player 1 input, checks if player 1 wins, checks player 2 input, checks if player 2 wins
+	while gameOver == False:
+		player_input(gameBoard, player1)
+		gameOver = win_check(gameBoard, player1)
+		if gameOver == True:
+			continue
+		player_input(gameBoard, player2)
+		gameOver = win_check(gameBoard, player2)
+		if gameOver == True:
+			continue
+
+	#Does the user want to play again, has 
+	replayGame = input("Do you want to play again? 'Y' or 'N': ")
+	correctReplaySlection = False
+	while correctReplaySlection == False:
+		if replayGame == 'Y':
+			correctReplaySlection = True
+			break
+		elif replayGame == 'N':
+			correctReplaySlection = False
+			break
+		else:
+			player1 = input("Please enter a valid selection. 'Y' or 'N': ")
+	replayGame = replay()
+	if replayGame == False:
+		break
+
+
+
+
+
+
+
+
